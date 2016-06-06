1b9182219f4039bebe725e6c2bcc604deac83041
Caleb Land <caleb@land.fm>
2016-06-05 21:52:29 -0400

    Add a group id and fix the license url

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject failjure-logging "0.1.0-SNAPSHOT"
+(defproject fm.caleb/failjure-logging "0.1.0-SNAPSHOT"
   :description "Some utilities for automatically logging failures in the Failjure library"
   :url "http://github.com/caleb/failjure-logging"
   :license {:name "The MIT License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [failjure "0.1.2"]])
